8b925f23f937d930f9a5b7214a35e86db8bfe7b1
liquidloose <ronaldlussier83@gmail.com>
2021-10-13 06:14:56 +0000

    Version 1.0

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 function game (rounds) {  
-
     function computerSelection () {
             let weapons = ['rock', 'paper', 'scissors'];
             let selection = weapons[Math.floor(Math.random() * weapons.length)];
         return selection; 
-        }
-
-    function playerSelection() {
-        let humanChoice = prompt("Choose your weapon: rock, paper or scissors", );
+    }
+    function playerSelection () {
+            let humanChoicePrompt = prompt("Choose your weapon: rock, paper or scissors", );
+            let humanChoice = humanChoicePrompt.toLowerCase(); 
+            console.log(humanChoice.substr(0,8));    
         return humanChoice;
     } 
-
     function playRound(playerSelection, computerSelection) {               
         let roundWinner =   playerSelection === 'rock'     && computerSelection === 'scissors' ? 'Human'
                           : playerSelection === 'paper'    && computerSelection === 'rock'     ? 'Human'
                           : playerSelection === 'scissors' && computerSelection === 'paper'    ? 'Human'
                           : playerSelection === 'scissors' && computerSelection === 'rock'     ? 'Machine'                              
                           : playerSelection === 'rock'     && computerSelection === 'paper'    ? 'Machine'                                                                                   
-                          : playerSelection === 'paper'    && computerSelection === 'scissors' ? 'Machine'  // Shoot!
-                          : 'It\'s a tie';  
+                          : playerSelection === 'paper'    && computerSelection === 'scissors' ? 'Machine' 
+                          : playerSelection      ===          computerSelection                ? 'Neither man, nor machine.' // Shoot!                          
+                          : 'I\'m sorry, there was a user input error.';  
         alert(
              `The computer chose:  ${computerSelection}
               The human chose:  ${playerSelection}
               The winner is:  ${roundWinner}`);   
     }    
-
     function playGame(rounds, playerSelection,computerSelection ) {
         let round = rounds;
         for(let i = 0; i < round; i++) {       
