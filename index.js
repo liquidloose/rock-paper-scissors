6c5c56573f965863a7b61dda509626333d8db046
liquidloose <ronaldlussier83@gmail.com>
2022-01-21 16:59:24 +0000

    just a small change

--- index.js
+++ index.js
@@ -1,109 +1,101 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-nested-ternary */
+
+/*
 function reset() {
   round = 1;
   humanScore = 0;
   computerScore = 0;
   changeRound(round);
-  changeScore(0, "Human");
-  changeScore(0, "Machine"); //triggers the changeScore(); function with simulated score for the output.
+  changeScore(0, 'Human');
+  changeScore(0, 'Machine'); // triggers the changeScore();
+   function with simulated score for the output.
 }
-
+*/
 function computerSelection() {
-  let weapons = ["rock", "paper", "scissors"];
-  let selection = weapons[Math.floor(Math.random() * weapons.length)];
-  return selection;
+  const weapons = ['rock', 'paper', 'scissors'];
+  const cpuSelection = weapons[Math.floor(Math.random() * weapons.length)];
+  return cpuSelection;
 }
 
-function playerSelection(computerSelection) {
-  let buttons = document.querySelectorAll("button");
+function playerSelection(cpuChoice) {
+  const buttons = document.querySelectorAll('button');
   buttons.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
       let playerSelection;
-      if (button.id === "rock") {
-        playerSelection = "rock";
+      if (button.id === 'rock') {
+        playerSelection = 'rock';
         changeRound(round);
-        playRound(playerSelection, computerSelection);
-      } else if (button.id === "scissors") {
+        playRound(playerSelection, cpuChoice);
+      } else if (button.id === 'scissors') {
         changeRound(round);
-        playerSelection = "scissors";
-        playRound(playerSelection, computerSelection);
-      } else if (button.id === "paper") {
+        playerSelection = 'scissors';
+        playRound(playerSelection, cpuChoice);
+      } else if (button.id === 'paper') {
         changeRound(round);
-        playerSelection = "paper";
-        playRound(playerSelection, computerSelection);
-      } 
+        playerSelection = 'paper';
+        playRound(playerSelection, cpuChoice);
+      }
     });
-
   });
 }
-
-
-
-
-
-function gameResults(computerSelection, roundWinner, playerSelection) {
-  const container = document.querySelector("#announcements");
-  
+function gameResults(computerSelection, roundWinner) {
+  const container = document.querySelector('#announcements');
   container.textContent = `FINAL WINNER:  ${roundWinner}`;
-
-
-
-
-
-
+  // eslint-disable-next-line no-undef
   container.appendChild(content);
-  let test = document.getElementById(`${computerSelection}-img`);
-  test.classList.add("computer-selection");
+  const test = document.getElementById(`${computerSelection}-img`);
+  test.classList.add('computer-selection');
 }
-
 function changeRound(round) {
-  if(round <= numOfRounds){
-  let fetchRound = document.getElementById("round");
-  fetchRound.textContent = `Current Round: ${round}`;
-  console.log(round);
+  if (round <= numOfRounds) {
+    const fetchRound = document.getElementById('round');
+    fetchRound.textContent = `Current Round: ${round}`;
   }
 }
-
 function changeScore(score, roundWinner) {
-  if (roundWinner === "Human" && humanScore <= numOfRounds) {
-    let fetchHumanScore = document.getElementById("human-score");
+  if (roundWinner === 'Human' && humanScore <= numOfRounds) {
+    const fetchHumanScore = document.getElementById('human-score');
     fetchHumanScore.textContent = `${humanScore}`;
-  } else if ((roundWinner === "Machine") && (computerScore <= numOfRounds)) {
-    let fetchComputerScore = document.getElementById("computer-score");
+  } else if (roundWinner === 'Machine' && computerScore <= numOfRounds) {
+    const fetchComputerScore = document.getElementById('computer-score');
     fetchComputerScore.textContent = `${computerScore}`;
-    let newItem = document.createElement("p");
-    newItem.id = "computer-score";
+    const newItem = document.createElement('p');
+    newItem.id = 'computer-score';
     newItem.innerHTML = `${score}`;
     fetchComputerScore.parentNode.replaceChild(newItem, fetchComputerScore);
   }
 }
 
 function roundResults(computerSelection, playerSelection, roundWinner) {
-  //anounces results
-  const container = document.querySelector("#announcements");
-  
+  // anounces results
+  const container = document.querySelector('#announcements');
+
   container.textContent = `
   Round winner: ${roundWinner}`;
   // highlights computer selection with green box-shadow
-  let test = document.getElementById(`${computerSelection}-img`);
-  test.classList.add("computer-selection");
+  const test = document.getElementById(`${computerSelection}-img`);
+  test.classList.add('computer-selection');
 
-  if (roundWinner === "Human") {
+  if (roundWinner === 'Human') {
     changeScore(humanScore, roundWinner);
-  } else if (roundWinner === "Machine") {
+  } else if (roundWinner === 'Machine') {
     changeScore(computerScore, roundWinner);
-  }else {
+  } else {
     changeRound(round);
   }
 }
 
 function winnerCheck(computerSelection, playerSelection, roundWinner) {
-  if (humanScore == numOfRounds) {
+  if (humanScore === numOfRounds) {
     changeScore(humanScore, roundWinner);
-    gameResults(playerSelection, roundWinner);    
+    gameResults(playerSelection, roundWinner);
+    // eslint-disable-next-line no-undef
     button.disable();
-  } else if (computerScore == numOfRounds) {
+  } else if (computerScore === numOfRounds) {
     changeScore(computerScore, roundWinner);
-    gameResults(computerSelection, roundWinner);    
+    gameResults(computerSelection, roundWinner);
   } else {
     roundResults(computerSelection, playerSelection, roundWinner);
     round++;
@@ -111,10 +103,10 @@ function winnerCheck(computerSelection, playerSelection, roundWinner) {
 }
 
 function gameTally(computerSelection, playerSelection, roundWinner) {
-  if (roundWinner === "Machine") {
+  if (roundWinner === 'Machine') {
     computerScore += 1;
     winnerCheck(computerSelection, playerSelection, roundWinner);
-  } else if (roundWinner === "Human") {
+  } else if (roundWinner === 'Human') {
     humanScore += 1;
     winnerCheck(computerSelection, playerSelection, roundWinner);
   } else {
@@ -124,28 +116,27 @@ function gameTally(computerSelection, playerSelection, roundWinner) {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let roundWinner =
-    playerSelection === "rock" && computerSelection === "scissors"
-      ? "Human"
-      : playerSelection === "paper" && computerSelection === "rock"
-      ? "Human"
-      : playerSelection === "scissors" && computerSelection === "paper"
-      ? "Human"
-      : playerSelection === "scissors" && computerSelection === "rock"
-      ? "Machine"
-      : playerSelection === "rock" && computerSelection === "paper"
-      ? "Machine"
-      : playerSelection === "paper" && computerSelection === "scissors"
-      ? "Machine"
-      : playerSelection === computerSelection
-      ? "Not man, nor machine." // Shoot!
-      : "I'm sorry, there was a user input error.";
+  const roundWinner = playerSelection === 'rock' && computerSelection === 'scissors'
+    ? 'Human'
+    : playerSelection === 'paper' && computerSelection === 'rock'
+      ? 'Human'
+      : playerSelection === 'scissors' && computerSelection === 'paper'
+        ? 'Human'
+        : playerSelection === 'scissors' && computerSelection === 'rock'
+          ? 'Machine'
+          : playerSelection === 'rock' && computerSelection === 'paper'
+            ? 'Machine'
+            : playerSelection === 'paper' && computerSelection === 'scissors'
+              ? 'Machine'
+              : playerSelection === computerSelection
+                ? 'Not man, nor machine.' // Shoot!
+                : "I'm sorry, there was a user input error.";
 
   gameTally(computerSelection, playerSelection, roundWinner);
 }
 
 function game() {
-  let cpuChoice = computerSelection();
+  const cpuChoice = computerSelection();
   playerSelection(cpuChoice);
 }
 
